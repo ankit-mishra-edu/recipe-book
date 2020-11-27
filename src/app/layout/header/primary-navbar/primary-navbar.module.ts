411f750b9da55825e431ffa7f066c858e8ddd639
amishm766 <amishm766@gmail.com>
2020-11-27 14:50:52 +0530

    Basic site layout created.
    Modularised basic site layout has been created.

---
+++ src/app/layout/header/primary-navbar/primary-navbar.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimaryNavbarComponent } from './primary-navbar.component';
+
+@NgModule({
+  declarations: [PrimaryNavbarComponent],
+  imports: [CommonModule],
+  exports: [PrimaryNavbarComponent],
+})
+export class PrimaryNavbarModule {}
